e6c53d06e43e87f3609cd287207aeee6fcd6a526
Milan Biranwar <milanbiranwar06@gmail.com>
2022-08-28 19:48:57 +0530

    Program for the State Census CSV File if incorrect Returns a custom Exception

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -56,7 +56,15 @@ namespace CensualAnalyserTest
             Assert.AreEqual(29, totalRecord.Count);
             Assert.AreEqual(37, stateRecord.Count);
         }
-       
+        [Test]
+        public void GivenWrongIndianCensusDataFile_WhenRead_ShouldReturnCustomException()
+        {
+            var censusException = Assert.Throws<CensusAnalyserException>(() => censusAnalyser.LoadCensusData(wrongIndianStateCensusFilePath, Country.INDIA, indianStateCensusHeaders));
+            var stateException = Assert.Throws<CensusAnalyserException>(() => censusAnalyser.LoadCensusData(wrongHeaderStateCodeFilePath, Country.INDIA, indianStateCodeHeaders));
+            Assert.AreEqual(CensusAnalyserException.ExceptionType.INCORRECT_HEADER, censusException.eType);
+            Assert.AreEqual(CensusAnalyserException.ExceptionType.INCORRECT_HEADER, stateException.eType);
+        }
+
     }
 
 }
